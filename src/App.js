c2161f0b7944be43183b88f831d0358bb8dd0c03
y-yanagii <yyanagisawa555@gmail.com>
2021-07-01 22:08:55 +0900

    learn state and state set

--- src/App.js
+++ src/App.js
@@ -1,62 +1,38 @@
-// import React, { Component } from "react";
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 
-// クラスコンポーネントの書き方
-// class App extends Component {
-//   render() {
-//     // const greeting = "Hi, Tom!";
-//     // 文法上reactではクラス付与はclassNameとする。変数展開は{変数名}
-//     // キャメルケースで書くことclassName, onClick
-//     // const dom = <h1 className="foo">{greeting}</h1>
-//     // return dom;
+const App = () => (<Counter></Counter>)
 
-//     // Reactの制約として1つのタグでしか返せない（Vueのコンポーネント規約と同様）
-//     // vueでいうtemplateはReact.Fragment
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar" onClick={() => {console.log("I am clicked.")}}>bar</label>
-//         <input type="text" onChange={() => {console.log("On Change !!")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
+class Counter extends Component {
+  // コンポーネントの初期化時、コンストラクターメソッドが呼び出される
+  constructor(props) {
+    super(props) // 親クラスで初期化処理
+    // stateにcountをセット
+    this.state = { count: 0 }
+  }
 
-// 関数コンポーネントの書き方
-const App = () => {
-  // ユーザ情報
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 3 },
-  ];
+  // state.countに+1
+  handlePlusButton = () => {
+    // stateの中身を変更する場合setState(再レンダリングされる(下のrender(){}が呼ばれている))
+    this.setState({ count: this.state.count + 1 });
+  }
 
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          // propsにname、ageを渡す
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
-}
+  // state.countを-1
+  handleMinusButton= () => {
+    // stateの中身を変更する場合setState(再レンダリングされる(下のrender(){}が呼ばれている))
+    this.setState({ count: this.state.count - 1 });
+  }
 
-// propsを受け取るコンポーネント
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
-}
-
-// default propsを指定propsに値がない場合設定する
-// User.defaultProps = {
-//   age: 1
-// };
-
-// propsの型を定義する(型通りじゃないとコンソールでWarningが出る)
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired, // isRequiredは必須チェック
+  render() {
+    console.log("render");
+    // console.log(this.state); // stateの初期値nullだがCounterクラスのコンストラクタで初期化しているため{count: 0}が入る
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={ this.handlePlusButton }>+1</button>
+        <button onClick={ this.handleMinusButton }>-1</button>
+      </React.Fragment> 
+    )
+  }
 }
 
 export default App;
